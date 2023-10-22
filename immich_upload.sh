774d7bc9be9737a2988972da6eee3d2bf6ed71c0
AlteSockenSuppe <mh@schrecknet.ch>
2023-10-22 14:04:58 +0200

    run shellcheck

--- immich_upload.sh
+++ immich_upload.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
-source ~/.config/immich_upload.conf
+source $HOME/.config/immich_upload.conf
 
 if [[ "$1" == "f" ]]; then
-    immich upload --yes --key "$key" --server "$server" "$2"
+    immich upload --yes --key "$key" --server "$api" "$2"
 elif [[ "$1" == "d" ]]; then
-    immich upload --yes --key "$key" --server "$server" --recursive "$2"
+    immich upload --yes --key "$key" --server "$api" --recursive "$2"
 elif [[ "$1" == "a" ]]; then
-    immich upload --yes --key "$key" --server "$server" --album --recursive "$2"
+    immich upload --yes --key "$key" --server "$api" --album --recursive "$2"
 else
     echo "use:"
     echo "* 'f' for foto"
